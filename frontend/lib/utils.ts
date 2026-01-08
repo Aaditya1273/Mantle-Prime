eae9f3668364514edd9b7eeb8454c0ee1f2b1719
[Aaditya1273] <aadityaiitian1515@gmail.com>
2026-01-08 09:46:35 +0530

    Mantle Network

---
+++ frontend/lib/utils.ts
@@ -0,0 +1,32 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatAddress(address: string): string {
+  return `${address.slice(0, 6)}...${address.slice(-4)}`
+}
+
+export function formatNumber(num: number, decimals: number = 2): string {
+  return new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: decimals,
+    maximumFractionDigits: decimals,
+  }).format(num)
+}
+
+export function formatCurrency(amount: number, currency: string = 'USD'): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: currency,
+  }).format(amount)
+}
+
+export function formatPercentage(value: number): string {
+  return `${(value * 100).toFixed(2)}%`
+}
+
+export function shortenHash(hash: string, length: number = 6): string {
+  return `${hash.slice(0, length)}...${hash.slice(-length)}`
+}
